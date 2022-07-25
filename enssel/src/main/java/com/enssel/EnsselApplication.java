ab837a1f72aee6838847da29d47d6b665276deb0
zni94 <87021422+zni94@users.noreply.github.com>
2022-07-25 13:45:55 +0900

    Enssel Web 0725

--- enssel/src/main/java/com/enssel/EnsselApplication.java
+++ enssel/src/main/java/com/enssel/EnsselApplication.java
@@ -2,12 +2,15 @@ package com.enssel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class EnsselApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EnsselApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EnsselApplication.class, args);
+    }
 }
